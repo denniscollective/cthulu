e0d7ec157dcf942021862dfe010df6741bcd9b01
Dennis Collective <dennis.collective@gmail.com>
2017-06-26 13:55:29 -0600

    Seperate configuration from server initialization

--- history/lib/history.ex
+++ history/lib/history.ex
@@ -3,14 +3,16 @@ defmodule History do
   require Logger
 
   def start(_type, _args) do
-   config = %History.Config{}
+    config = %History.Config{}
+    History.start_server(config.port)
+  end
 
+  def start_server(port) do
    children = [
-     Plug.Adapters.Cowboy.child_spec(:http, History.WebServer, [],
-     port: config.port)
+     Plug.Adapters.Cowboy.child_spec(:http, History.WebServer, [], port: port)
    ]
 
-   Logger.info "Started application on #{config.port}"
+   Logger.info "Started application on #{port}"
 
    Supervisor.start_link(children, strategy: :one_for_one)
  end
